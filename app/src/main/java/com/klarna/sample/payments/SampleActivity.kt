a53721d9a20ff6331a6227e2c2efd6a03cdbffb2
Adrian Palmquist <adrian.palmquist@klarna.com>
2019-12-18 15:28:07 +0100

    2.0.8

--- app/src/main/java/com/klarna/sample/payments/SampleActivity.kt
+++ app/src/main/java/com/klarna/sample/payments/SampleActivity.kt
@@ -2,9 +2,9 @@ package com.klarna.sample.payments
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.klarna.mobile.sdk.api.payments.KlarnaPaymentView
 import com.klarna.mobile.sdk.api.payments.KlarnaPaymentViewCallback
 import com.klarna.mobile.sdk.api.payments.KlarnaPaymentsSDKError
