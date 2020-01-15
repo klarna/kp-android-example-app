668cba4837bf06372e03cfa7c352951f685d290e
Mahmoud Jafarinejad <mahmoud.jafarinejad@klarna.com>
2020-01-15 11:22:10 +0100

    -updated gradle and kotlin version -updated dependencies -added error messages -switched layout to constraintLayout -refactored classes and added error dialog

--- app/src/main/java/com/klarna/sample/payments/SampleActivity.kt
+++ app/src/main/java/com/klarna/sample/payments/SampleActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.klarna.mobile.sdk.api.payments.KlarnaPaymentView
 import com.klarna.mobile.sdk.api.payments.KlarnaPaymentViewCallback
@@ -17,64 +18,85 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class SampleActivity : AppCompatActivity(), KlarnaPaymentViewCallback {
-    private val paymentView by lazy { findViewById<KlarnaPaymentView>(R.id.paymentView) }
 
-
-    private val authorize by lazy { findViewById<Button>(R.id.authorize) }
-    private val finalize by lazy { findViewById<Button>(R.id.finalize) }
-    private val order by lazy { findViewById<Button>(R.id.create_order) }
+    private val klarnaPaymentView by lazy { findViewById<KlarnaPaymentView>(R.id.klarnaPaymentView) }
+    private val authorizeButton by lazy { findViewById<Button>(R.id.authorizeButton) }
+    private val finalizeButton by lazy { findViewById<Button>(R.id.finalizeButton) }
+    private val orderButton by lazy { findViewById<Button>(R.id.orderButton) }
 
     private var job: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_sample)
-        paymentView.category = PAY_LATER
+        initialize()
+        setupButtons()
+        klarnaPaymentView.category = PAY_LATER
+    }
 
-        authorize.setOnClickListener {
-            paymentView.authorize(true, null)
-        }
-
-        finalize.setOnClickListener {
-            paymentView.finalize(null)
-        }
-
-        order.setOnClickListener {
-            job = GlobalScope.launch {
-                val successful =
-                    OrderClient.instance.createOrder(it.tag as String, OrderPayload.defaultPayload).execute()
-                        .isSuccessful
-
-                if (successful) {
-                    startActivity(Intent(this@SampleActivity, OrderCompletedActivity::class.java))
-                    finish()
-                }
+    private fun initialize() {
+        job = GlobalScope.launch {
+            val sessionCall = OrderClient.instance.createCreditSession(OrderPayload.defaultPayload)
+            try {
+                sessionCall.execute().body()?.let { session ->
+                    runOnUiThread {
+                        klarnaPaymentView.initialize(session.client_token, "kp-sample://kp")
+                    }
+                } ?: showError(null)
+            } catch (exception: Exception) {
+                showError(exception.message)
             }
         }
+    }
 
-
+    private fun createOrder() {
         job = GlobalScope.launch {
-            OrderClient.instance.createCreditSession(OrderPayload.defaultPayload)
-                .execute()
-                .body()
-                ?.let {
-                    GlobalScope.launch (Dispatchers.Main) {
-                        paymentView.initialize(it.client_token, "kp-sample://kp")
+            val orderCall = OrderClient.instance.createOrder(orderButton.tag as String, OrderPayload.defaultPayload)
+            try {
+                val response = orderCall.execute()
+                if (response.isSuccessful) {
+                    runOnUiThread {
+                        startActivity(Intent(this@SampleActivity, OrderCompletedActivity::class.java))
+                        finish()
                     }
+                } else {
+                    showError(null)
                 }
+            } catch (exception: Exception) {
+                showError(exception.message)
+            }
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        paymentView.registerPaymentViewCallback(this)
+    private fun setupButtons() {
+        authorizeButton.setOnClickListener {
+            klarnaPaymentView.authorize(true, null)
+        }
+
+        finalizeButton.setOnClickListener {
+            klarnaPaymentView.finalize(null)
+        }
+
+        orderButton.setOnClickListener {
+            createOrder()
+        }
     }
 
-    override fun onPause() {
-        super.onPause()
-        paymentView.unregisterPaymentViewCallback(this)
-        job?.cancel()
+    private fun showError(message: String?) {
+        runOnUiThread {
+            val alertDialog = AlertDialog.Builder(this).create()
+            alertDialog.setMessage(message ?: getString(R.string.error_general))
+            alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK") { dialog, _ ->
+                dialog.dismiss()
+            }
+            alertDialog.show()
+        }
+    }
+
+    private fun runOnUiThread(action: () -> Unit) {
+        GlobalScope.launch(Dispatchers.Main) {
+            action.invoke()
+        }
     }
 
     override fun onInitialized(view: KlarnaPaymentView) {
@@ -82,15 +104,20 @@ class SampleActivity : AppCompatActivity(), KlarnaPaymentViewCallback {
     }
 
     override fun onLoaded(view: KlarnaPaymentView) {
-        authorize.isEnabled = true
+        authorizeButton.isEnabled = true
     }
 
     override fun onLoadPaymentReview(view: KlarnaPaymentView, showForm: Boolean) {}
 
-    override fun onAuthorized(view: KlarnaPaymentView, approved: Boolean, authToken: String?, finalizedRequired: Boolean?) {
-        finalize.isEnabled = finalizedRequired ?: false
-        order.isEnabled = approved && !(finalizedRequired ?: false)
-        order.tag = authToken
+    override fun onAuthorized(
+        view: KlarnaPaymentView,
+        approved: Boolean,
+        authToken: String?,
+        finalizedRequired: Boolean?
+    ) {
+        finalizeButton.isEnabled = finalizedRequired ?: false
+        orderButton.isEnabled = approved && !(finalizedRequired ?: false)
+        orderButton.tag = authToken
     }
 
     override fun onReauthorized(view: KlarnaPaymentView, approved: Boolean, authToken: String?) {}
@@ -98,15 +125,24 @@ class SampleActivity : AppCompatActivity(), KlarnaPaymentViewCallback {
     override fun onErrorOccurred(view: KlarnaPaymentView, error: KlarnaPaymentsSDKError) {
         println("An error occurred: ${error.name} - ${error.message}")
         if (error.isFatal) {
-            paymentView.visibility = View.INVISIBLE
+            klarnaPaymentView.visibility = View.INVISIBLE
         }
-
     }
 
     override fun onFinalized(view: KlarnaPaymentView, approved: Boolean, authToken: String?) {
-        order.isEnabled = approved
-        order.tag = authToken
+        orderButton.isEnabled = approved
+        orderButton.tag = authToken
     }
 
+    override fun onResume() {
+        super.onResume()
+        klarnaPaymentView.registerPaymentViewCallback(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        klarnaPaymentView.unregisterPaymentViewCallback(this)
+        job?.cancel()
+    }
 
 }
--- app/src/main/java/com/klarna/sample/payments/api/OrderClient.kt
+++ app/src/main/java/com/klarna/sample/payments/api/OrderClient.kt
@@ -10,21 +10,15 @@ object OrderClient {
     private val user = "<please enter here>"
     private val password = "<please enter here>"
 
-    val instance by lazy {
+    val instance: OrderService by lazy {
 
-        val builder = OkHttpClient.Builder()
-            .addInterceptor(BasicAuthInterceptor(user, password))
+        val builder = OkHttpClient.Builder().addInterceptor(BasicAuthInterceptor(user, password))
         val okHttpClient = builder.build()
-
-
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.playground.klarna.com/payments/v1/")
             .addConverterFactory(GsonConverterFactory.create())
             .client(okHttpClient)
             .build()
-
         retrofit.create(OrderService::class.java)
     }
-
-
 }
